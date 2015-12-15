8d4bf866afd2d28b1a87e5f43be7cc859de01af8
Aurélien Lebeau <aurelienlebeau@mbp-de-aurelien.lan>
2015-12-15 10:30:45 +0100

    Webview and segues creation

---
+++ Code/ExpoSup/WebViewController.h
@@ -0,0 +1,20 @@
+//
+//  WebViewController.h
+//  exposup
+//
+//  Created by Aurélien Lebeau on 15/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "LanguageManagement.h"
+#import "SectionParentViewController.h"
+#import "Alerts.h"
+#import "Labels.h"
+#import <Foundation/Foundation.h>
+
+@interface WebViewController : SectionParentViewController
+
+@property (assign, nonatomic) NSString *fileName;
+
+@end
---
+++ Code/ExpoSup/WebViewController.m
@@ -0,0 +1,13 @@
+//
+//  WebViewController.m
+//  exposup
+//
+//  Created by Aurélien Lebeau on 15/12/15.
+//
+//
+
+#import "WebViewController.h"
+
+@implementation WebViewController
+
+@end
